2d44711a0f2d6ccc14c83e56619d204a1aa614cd
Sanjay Vala <sanjayvala1999@gmail.com>
2023-12-22 15:06:50 +0530

    Initial commit

---
+++ Graphql-Postgres-master/resolvers.js
@@ -0,0 +1,109 @@
+const bcrypt = require("bcryptjs");
+const { Op } = require("sequelize");
+
+const resolvers = {
+  Query: {
+    async user(root, { id }, { models }, info) {
+      try {
+        const user = await models.User.findByPk(id);
+
+        if (!user) {
+          throw new Error("No Such User");
+        }
+
+        return user;
+      } catch (error) {
+        console.log("Error", error);
+      }
+    },
+    async events(root, { id }, { models }, info) {
+      try {
+        const events = await models.Events.findByPk(id);
+
+        if (!events) {
+          throw new Error("No Such Error");
+        }
+
+        return events;
+      } catch (error) {
+        console.log("Error", error);
+      }
+    },
+    async allusers(root, args, { models }, info) {
+      try {
+        const users = await models.User.findAll({
+          include: {
+            model: models.Events,
+            as: "events",
+          },
+        });
+
+        if (!users) {
+          throw new Error("No users found");
+        }
+
+        return users;
+      } catch (error) {
+        console.log("Error", error);
+      }
+    },
+    async allevents(root, args, { models }, info) {
+      try {
+        const events = await models.Events.findAll({
+          where: {
+            id: {
+              [Op.gt]: 2,
+            },
+          },
+        });
+        if (!events) {
+          throw new Error("No Events Found");
+        }
+
+        return events;
+      } catch (error) {
+        console.log("Error", error);
+      }
+    },
+  },
+  Mutation: {
+    async createUser(root, { email, password }, { models }, info) {
+      const user = await models.User.create({
+        email,
+        password: await bcrypt.hash(password, 10),
+      });
+      return user;
+    },
+    async createEvents(root, { name, userId }, { models }, info) {
+      const events = await models.Events.create({
+        name,
+        userId,
+      });
+      return events;
+    },
+    async createEventsInvites(root, { invites, id }, { models }, info) {
+      try {
+        const events = await models.Events.update(
+          {
+            invites,
+          },
+          {
+            where: {
+              id: id,
+            },
+          }
+        );
+
+        if (!events) {
+          throw new Error("Oops something went wrong");
+        }
+
+        return events;
+      } catch (error) {
+        console.log("Error", error);
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ Graphql-Postgres-master/schema.js
@@ -0,0 +1,32 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    user(id: Int!): User!
+    allusers: [User!]
+    events(id: Int!): Events
+    allevents: [Events!]
+  }
+
+  type User {
+    id: Int!
+    email: String!
+    password: String!
+    events: [Events!]
+  }
+
+  type Events {
+    id: Int!
+    name: String!
+    invites: [String!]
+    user: User!
+  }
+
+  type Mutation {
+    createUser(email: String!, password: String!): User!
+    createEvents(name: String!, userId: Int!): Events!
+    createEventsInvites(invites: [String!]!, id: Int!): [Int]!
+  }
+`;
+
+module.exports = typeDefs;
